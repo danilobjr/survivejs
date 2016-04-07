a22c37851e3887ac3bbf0816975672fd24cc0f23
Danilo Barros <danilobjr@gmail.com>
2016-04-07 01:06:48 -0300

    entenring on edit mode on a note

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
 
+const styles = {
+    span: {
+        display: 'block'
+    },
+    input: {
+        display: 'none'
+    },
+    isInEditionMode: {
+        span: {
+            display: 'none'
+        },
+        input: {
+            display: 'block'
+        }
+    }
+}
+
 class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isInEditionMode: false
+        };
+    }
+    
     render() {
+        var spanStyle = (this.state.isInEditionMode) ? styles.isInEditionMode.span : styles.span;
+        var inputStyle = (this.state.isInEditionMode) ? styles.isInEditionMode.input : styles.input;
+        
         return (
-            <li>
-                {this.props.task}
+            <li onClick={this.enterEditionMode.bind(this)}>
+                <span style={spanStyle}>{this.props.task}</span>
+                <input
+                    style={inputStyle} 
+                    ref="taskInput"
+                    type="text" 
+                    defaultValue={this.props.task} 
+                    onKeyDown={this.saveTask.bind(this)} 
+                    onBlur={this.saveTask.bind(this)}
+                />
             </li>
         );
+    }
+    
+    enterEditionMode() {        
+        this.setState({ isInEditionMode: true });
+        this.refs.taskInput.focus();
+    }
+    
+    saveTask(e) {
+        let enterKey = 13;
+        
+        if (e.keyCode === enterKey || e.type === 'blur') {
+            this.setState({ isInEditionMode: false });
+        }
     }
 }
 
